cd31dcf28e065ce9eee98231130972c9d7ed2ff2
natasha-reznikova <reznikova0586@gmail.com>
2021-05-20 00:11:13 +0300

    готов 2й модуль

--- js/modal2.js
+++ js/modal2.js
@@ -343,16 +343,173 @@
 // console.log(filterArray([12, 24, 8, 41, 76], 20));
 
 
-// Задание 24. Метод includes()
+// // Задание 24. Метод includes()
 
- function checkFruit(fruit) {
-  const fruits = ["apple", "plum", "pear", "orange"];
-console.log(fruits.includes(fruit))
+//  function checkFruit(fruit) {
+//   const fruits = ["apple", "plum", "pear", "orange"];
+// console.log(fruits.includes(fruit))
 
-// Change this line
-}
-console.log(checkFruit("plum"));
-console.log(checkFruit("mandarin"));
-console.log(checkFruit("pear"));
-console.log(checkFruit("Pear"));
-console.log(checkFruit("apple"));
+// // Change this line
+// }
+// console.log(checkFruit("plum"));
+// console.log(checkFruit("mandarin"));
+// console.log(checkFruit("pear"));
+// console.log(checkFruit("Pear"));
+// console.log(checkFruit("apple"));
+
+
+//  // Задание 25. Задача: общие элементы
+
+// function getCommonElements(array1, array2) {
+//   // Change code below this line
+// const unitElements = [];
+
+// for (const element of array1) {
+//   // console.log(array1);
+//   // console.log(array2)
+
+//   if (array2.includes(element)) {
+//     unitElements.push(element);
+//   }
+
+// }
+//   return unitElements;
+
+//  // Change code above this line
+// }
+
+// console.log(getCommonElements([1, 2, 3], [2, 4]));
+// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+// console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
+
+
+//  // Задание 26. Цикл for...of
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+
+//   for (const item of order) {
+//     total += item;
+//   }
+
+//   // Change code above this line
+//   return total;
+// }
+// calculateTotalPrice([12, 85, 37, 4]);
+// calculateTotalPrice([164, 48, 291]);
+// calculateTotalPrice([412, 371, 94, 63, 176]);
+// calculateTotalPrice([]);
+
+// // Задание 27. Зачада: фильтрация массива чисел 2.0
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   const filteredNumbers = [];
+
+//   for (const number of numbers) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+
+//   return filteredNumbers;
+//   // Change code above this line
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+
+// // Задание 28. Оператор %
+// // Change code below this line
+// const a = 3 % 3;
+// const b = 4 % 3;
+// const c = 11 % 4;
+// const d = 12 % 7;
+// const e = 8 % 6;
+
+// console.log(a, b, c, d, e)
+
+// // Задание 29. Задача: чётные числа
+
+// function getEvenNumbers(start, end) {
+//    // Change code below this line
+// const evenNumbers = [];
+  
+  
+// for (let i = start; i <= end; i += 1) {
+// if (i % 2 === 0) {
+// evenNumbers.push(i);
+// }
+// }
+// return evenNumbers;
+//     // Change code above this line
+//   }
+
+// console.log(getEvenNumbers(2, 5));
+// console.log(getEvenNumbers(3, 11));
+// console.log(getEvenNumbers(6, 12));
+// console.log(getEvenNumbers(8, 8));
+// console.log(getEvenNumbers(7, 7));
+
+
+// // Задание 30. Оператор break
+// const start = 6;
+// const end = 27;
+// let number;
+
+// for (let i = start; i < end; i += 1) {
+//   if (i % 5 === 0) {
+//     number = i;
+// break;
+//   }
+// }
+
+
+// // Задание 31. Оператор break vs return в функции
+// function findNumber(start, end, divisor) {
+//   // Change code below this line
+//   let number;
+
+//   for (let i = start; i < end; i += 1) {
+//     if (i % divisor === 0) {
+//   return i;
+//     }
+//   }
+//   // Change code above this line
+// }
+
+// console.log(findNumber(2, 6, 5));
+// console.log(findNumber(8, 17, 3));
+// console.log(findNumber(6, 9, 4));
+// console.log(findNumber(16, 35, 7));
+
+
+// Задание 3. Задача: функция includes()
+
+function includes(array, value) {
+  // Change code below this line
+
+  for (const arr of array) {
+    if (arr === value) {
+      return true;
+    }
+    
+  }
+  return false;
+  }
+
+  
+
+  // Change code above this line
+
+includes([1, 2, 3, 4, 5], 3);
+console.log(includes([1, 2, 3, 4, 5], 17));
+console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter"));
+console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus"));
+console.log(includes(["apple", "plum", "pear", "orange"], "plum"));
+console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
